6564a78cf78c302aa0ce61732bfd705551d9af4e
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2024-11-05 11:03:49 +0100

    Add more filter options

--- src/content/fetchBlogPosts.ts
+++ src/content/fetchBlogPosts.ts
@@ -5,7 +5,12 @@ import { Post, Tag } from '@/payload/payload-types';
 type FetchPostsOptions = {
   limit?: number;
   page?: number;
-  tag?: string | Tag;
+  tags?: (string | Tag)[];
+  ignoredPosts?: (string | Post)[];
+};
+
+const contentToId = (item: string | Tag | Post) => {
+  return typeof item === 'string' ? item : item.id;
 };
 
 const fetchBlogPosts = async (
@@ -16,23 +21,35 @@ const fetchBlogPosts = async (
   const limit = options.limit && options.limit >= 0 ? options.limit : 10;
   const page = options.page && options.page > 0 ? options.page : 1;
 
-  const tagId =
-    typeof options.tag === 'string' ? options.tag : options?.tag?.id;
+  const tagIds = (options.tags || []).map(contentToId);
+  const ignoredPosts = (options.ignoredPosts || []).map(contentToId);
 
-  const where = tagId
-    ? {
-        tags: {
-          in: [tagId],
-        },
-      }
-    : undefined;
+  const where = [];
+
+  if (tagIds.length > 0) {
+    where.push({
+      tags: {
+        in: tagIds,
+      },
+    });
+  }
+
+  if (ignoredPosts.length > 0) {
+    where.push({
+      id: {
+        not_in: ignoredPosts,
+      },
+    });
+  }
 
   return client.find({
     collection: 'posts',
     sort: '-publishedAt',
     limit,
     page,
-    where,
+    where: {
+      and: where,
+    },
   });
 };
 
