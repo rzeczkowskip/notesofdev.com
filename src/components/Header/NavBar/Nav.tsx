20540ee1790c09ca8b5240767791b377acb758b0
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2024-11-05 11:38:12 +0100

    Fix navbar menu links

--- src/components/Header/NavBar/Nav.tsx
+++ src/components/Header/NavBar/Nav.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { Nav as NavItems } from '@/payload/payload-types';
+import extractLinkFieldProps from '@/utils/extractLinkFieldProps';
 
 type NavProps = {
   items: NonNullable<NavItems>;
@@ -8,15 +9,17 @@ type NavProps = {
 
 const Nav: React.FC<NavProps> = ({ items }) => (
   <>
-    {items.map(({ label }) => (
-      <Link
-        key={`${label}${label}`}
-        href={label}
-        className={'lg:py-2 hover:underline hover:text-primary-11'}
-      >
-        {label}
-      </Link>
-    ))}
+    {items.map(({ id, label, link }) =>
+      link ? (
+        <Link
+          key={`${id}`}
+          className={'lg:py-2 hover:underline hover:text-primary-11'}
+          {...extractLinkFieldProps(link)}
+        >
+          {label}
+        </Link>
+      ) : null,
+    )}
   </>
 );
 
