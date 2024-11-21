5750f3e6d7407cfa2f595a1f5dd9a936dfc26beb
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2024-11-21 10:22:19 +0100

    Remove deprecated code

--- src/payload/client/index.ts
+++ src/payload/client/index.ts
@@ -1,13 +1,11 @@
 import { CachedPayload } from '@payload-enchants/cached-local-api';
-import { getPayloadHMR } from '@payloadcms/next/utilities';
-import { draftMode, headers } from 'next/headers';
-import type { Payload } from 'payload';
+import { getPayload as getPayloadClient, Payload } from 'payload';
 import { getCachedPayload } from '@/payload/cache';
 import { Admin } from '@/payload/payload-types';
 import payloadConfig from '@payload-config';
 
 export const getUncachedPayload = async (): Promise<Payload> => {
-  return getPayloadHMR({ config: payloadConfig });
+  return getPayloadClient({ config: payloadConfig });
 };
 
 export const getPayload = async (): Promise<CachedPayload> => {
