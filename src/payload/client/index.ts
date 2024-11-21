01bacf9f269e45e08aab393c5d2069cd34781035
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2024-11-21 10:28:31 +0100

    Cache client

--- src/payload/client/index.ts
+++ src/payload/client/index.ts
@@ -1,13 +1,14 @@
 import { CachedPayload } from '@payload-enchants/cached-local-api';
 import { getPayload as getPayloadClient, Payload } from 'payload';
+import { cache } from 'react';
 import { getCachedPayload } from '@/payload/cache';
-import { Admin } from '@/payload/payload-types';
 import payloadConfig from '@payload-config';
 
 export const getUncachedPayload = async (): Promise<Payload> => {
   return getPayloadClient({ config: payloadConfig });
 };
 
-export const getPayload = async (): Promise<CachedPayload> => {
-  return getCachedPayload(await getUncachedPayload());
-};
+export const getPayload = cache(
+  async (): Promise<CachedPayload> =>
+    getCachedPayload(await getUncachedPayload()),
+);
