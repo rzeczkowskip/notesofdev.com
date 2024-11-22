c81809029cf058189275768d3afe4195c8e2a46d
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2024-11-22 17:03:55 +0100

    Remove pagination from latest posts page

--- src/components/BlogLatestPosts/BlogLatestPosts.tsx
+++ src/components/BlogLatestPosts/BlogLatestPosts.tsx
@@ -1,25 +1,20 @@
 import BlogPostsList from '@/components/BlogPostsList/BlogPostsList';
-import Pagination from '@/components/Pagination';
 import Prose from '@/components/Prose';
 import Section from '@/components/Section';
-import { STATIC_ROUTES } from '@/contants';
 import fetchBlogPosts from '@/content/fetchBlogPosts';
 import type { Tag } from '@/payload/payload-types';
 
 type BlogLatestPostsProps = {
   postCount?: number;
-  page?: number;
   tags?: (string | Tag)[];
 };
 
 const BlogLatestPosts = async ({
   postCount = 5,
-  page = 1,
   tags,
 }: BlogLatestPostsProps) => {
   const posts = await fetchBlogPosts({
     limit: postCount,
-    page: page,
     tags,
   });
 
@@ -27,21 +22,9 @@ const BlogLatestPosts = async ({
     return <Prose>No posts found.</Prose>;
   }
 
-  const { hasNextPage, hasPrevPage } = posts;
-
   return (
     <Section>
       <BlogPostsList posts={posts.docs} />
-
-      <Pagination
-        className="mt-10"
-        hasNextPage={hasNextPage}
-        hasPrevPage={hasPrevPage}
-        nextPageLabel={'Older posts'}
-        prevPageLabel={'Newer posts'}
-        page={page}
-        generatePageHref={(p) => `/${STATIC_ROUTES.Blog}/?page=${p}`}
-      />
     </Section>
   );
 };
